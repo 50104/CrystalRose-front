5ac5fafd47a881d5a8a42eb8b254f308b7a963e3
50104 <solaya7@naver.com>
2024-05-27 20:27:38 +0900

    스프링부트 연동 설정(연동 테스트 완료)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Main from './components/layout/Main';
+
+function App() {
+  return (
+    <div className="web">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
