0b1f94c33b190157d54ebedea42efcf32af39da4
50104 <solaya7@naver.com>
2025-04-11 20:20:52 +0900

    Refactor : 401 응답 시 /get-access로 리다이렉트 응답 인터셉터 추가

--- src/utils/userInfo/api/axiosInstance.js
+++ src/utils/userInfo/api/axiosInstance.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   withCredentials: true,
 });
 
-// 요청 전 인터셉터로 토큰을 최신으로 설정
+// 요청 전에 access 토큰을 헤더에 세팅
 instance.interceptors.request.use(
   async (config) => {
     const token = localStorage.getItem('access');
@@ -15,6 +15,18 @@ instance.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+// 응답 에러 처리: access 토큰 만료 시 /getAccess로 리다이렉트
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      console.warn('Access 토큰 만료, getAccess 요청');
+      window.location.href = '/getAccess';
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
