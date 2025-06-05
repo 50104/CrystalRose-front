5007e2125cffdbfaaec28d9a548b316abe876966
50104 <solaya7@naver.com>
2025-06-05 23:32:13 +0900

    Feat: Header 사용자 표시 수정

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -9,17 +9,7 @@ import { GetUser } from '@utils/api/user';
 import './Header.css';
 
 const Header = () => {
-  const { isLogin, userId, userRole } = GetUser();
-
-  const getUserIdForDisplay = (id) => {
-    const prefixes = ['naver', 'kakao', 'google'];
-    for (const prefix of prefixes) {
-      if (id.startsWith(prefix)) return id.substring(6, 11);
-    }
-    return id;
-  };
-
-  const userIdForDisplay = getUserIdForDisplay(userId);
+  const { isLogin, userId, userRole, userNick } = GetUser();
 
   return (
     <div className="header_div">
@@ -35,13 +25,13 @@ const Header = () => {
           <>
             <div className='nav_userInfo'><a href="/memberList"><ul>회원 리스트</ul></a></div>
             {userRole === 'ROLE_ADMIN' && (
-              <div className="nav_userInfo"><a href="/admin"><ul>{userIdForDisplay}님 관리자 페이지</ul></a></div>
+              <div className="nav_userInfo"><a href="/admin"><ul>{userNick}님 관리자 페이지</ul></a></div>
             )}
             {userRole === 'ROLE_WRITER' && (
-              <div className="nav_userInfo"><a href="/writer"><ul>{userIdForDisplay}님 평론가 페이지</ul></a></div>
+              <div className="nav_userInfo"><a href="/writer"><ul>{userNick}님 평론가 페이지</ul></a></div>
             )}
             {userRole !== 'ROLE_ADMIN' && userRole !== 'ROLE_WRITER' && (
-              <div className="nav_userInfo"><a href="/mypage"><ul>{userIdForDisplay}님 마이페이지</ul></a></div>
+              <div className="nav_userInfo"><a href="/mypage"><ul>{userNick}님 마이페이지</ul></a></div>
             )}
             <div className="nav_userInfo"><a href="/favoriteList"><ul>즐겨찾기</ul></a></div>
             <div className="nav_userInfo cursor" onClick={logoutFunction}><ul>로그아웃</ul></div>
